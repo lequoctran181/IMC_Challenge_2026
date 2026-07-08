a87d16a725840e4c4f6d05f28eaddd5861c74440
Quoc Tran Anh Le <lequoctran181@gmail.com>
2026-07-09 06:41:32 +0700

    Prune fetched sources to 300 files

--- fetched_sources/kattis_19921918_0.000000_7.cpp
+++
@@ -1,6 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <iostream>
-using namespace std;
-int main(){string s((istreambuf_iterator<char>(cin)),{});if(s.empty())return 0;char*p=&s[0];long N=strtol(p,&p,10),M=strtol(p,&p,10);if(N==49985)puts("1 1\nv 0 0 0\nf 1 1 1");else fwrite(s.data(),1,s.size(),stdout);}
